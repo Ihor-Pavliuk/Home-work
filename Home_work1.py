49ffe231c66ed12974d3e4ba8c52b46f98c733c1
Ihor-Pavliuk <igor.pavliuk89@gmail.com>
2024-09-24 08:18:56 +0300

    Update Home_work1.py

--- Home_work1.py
+++ Home_work1.py
@@ -1,24 +1,26 @@
-# Декілька способів, можно ще модулі підв'язати, але це буде окремий файл
-print ('Hello world')
+# Декілька способів task2
+#Hello world, again!
+print ('Hello world, again!')
+#Hello world, again!
 text_1 = 'Hello world' 
 text_2 = 'again'
-print (text_1, text_2, sep=',', end='!\n')
-print ('{0},{1}' .format(text_1, text_2), end='!\n')
+print (text_1, text_2, sep=', ', end='!\n')
+#Hello world, again!
+print ('{0}, {1}' .format(text_1, text_2), end='!\n')
+#Hello world, again!
 print (f"{text_1}, {text_2}!\n\n")
 
-s = '#' # це не правильно написаний код, тому що сторонній особі нічого не зрозуміло, просто було цікаво чи можно це зробити в одній строці 
-print (f"{s}{s}{s}{s}{s}{s}{s}{s}{s}\n{s}\t{s}\n{s}\t{s}\n{s}\t{s}\n{s}{s}{s}{s}{s}{s}{s}{s}{s}\n\n{s}\t{s}\n{s}\t{s}\n{s}{s}{s}{s}{s}{s}{s}{s}{s}\n{s}\t{s}\n{s}\t{s}\n")
-# ось домашка
-print (f"{s}{s}{s}{s}{s}{s}{s}{s}{s}\n{s}\t{s}\n{s}\t{s}\n{s}\t{s}\n{s}{s}{s}{s}{s}{s}{s}{s}{s}\n")
-print (f"{s}\t{s}\n{s}\t{s}\n{s}{s}{s}{s}{s}{s}{s}{s}{s}\n{s}\t{s}\n{s}\t{s}\n")
-# перше виконання моє, цього завдання:
-print('#','#','#','#','#','#','#','#','#', end='\n') 
-print('#','#', sep='\t\t', end='\n')
-print('#','#', sep='\t\t', end='\n')
-print('#','#', sep='\t\t', end='\n')
-print('#','#','#','#','#','#','#','#','#', end='\n\n\n')
-print('#','#', sep='\t\t', end='\n')
-print('#','#', sep='\t\t', end='\n')
-print('#','#','#','#','#','#','#','#','#', end='\n')
-print('#','#', sep='\t\t', end='\n')
-print('#','#', sep='\t\t', end='\n')
+#task3
+print ("""#########\n#\t#\n#\t#\n#\t#\n#########\n""")
+print ("""#\t#\n#\t#\n#########\n#\t#\n#\t#\n""")
+#або так
+print ("""#########
+#\t#
+#\t#
+#\t#
+#########\n""")
+print ('''#\t#
+#\t#
+#########
+#\t#
+#\t#''')
