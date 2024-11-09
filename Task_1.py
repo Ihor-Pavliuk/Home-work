529d6483411fe466c736b628d108920b29bfb3fc
Ihor-Pavliuk <igor.pavliuk89@gmail.com>
2024-11-09 10:12:28 +0200

    Hw14

--- Task_1.py
+++ Task_1.py
@@ -1,4 +1,14 @@
-import Task_module_1 
-Task_module_1.hello()
-from Task_module_1 import hello
-hello()
+def logger(func):
+    def wrapper(*args):
+        print(f"Calling {func.__name__} with args:{args}")
+    return wrapper
+
+@logger
+def add(a, b):
+    return a + b
+@logger
+def square_all(*args):
+    return [arg ** 2 for arg in args]
+
+add(4, 5)
+square_all(1, 2, 3, 4, 5)
--- Task_2.py
+++ Task_2.py
@@ -1,10 +1,18 @@
-import sys
+def stop_words(words: list):
+    def decorator(func):
+        def wrapper(*args):
+            result = func(*args)
+            for i in words:
+                result = result.replace(i, '*')
+            return result
+        return wrapper
+    return decorator
+@stop_words(['pepsi', 'BMW'])
 
-sys.path.append("C:\\Users\\User\\OneDrive\\Робочий стіл\\Python\\Beetroot_128\\Home_work_9\\sys.py")
-for i in sys.path:
-    del sys.path[0]
-    del sys.path[0]
-    del sys.path[0]
-print (sys.path)
-# Я можу видалити всі шляхи до sys, окрім пари, з дерикторії в якій я запускаю модулі
-# на функціонал це ніяк не вплинуло
+def create_slogan(name: str) -> str:
+    return f"{name} drinks pepsi in his brand new BMW!"
+     
+        
+
+assert create_slogan("Steve") == "Steve drinks * in his brand new *!"
+print(create_slogan("Toy"))
