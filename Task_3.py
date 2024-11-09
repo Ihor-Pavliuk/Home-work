571cfe8f9d97688e37febe659543081ed348376d
Ihor-Pavliuk <igor.pavliuk89@gmail.com>
2024-11-09 10:13:26 +0200

    Hw15

--- Task_3.py
+++ Task_3.py
@@ -1,10 +1,93 @@
-from mymod import test, test_2
+#TV controller
 
-test("data.txt")
-test_2("data.txt")
+#Create a simple prototype of a TV controller in Python. It’ll use the following commands:
 
-#Does your PYTHONPATH need to include the directory where you created mymod.py?
-#Так, потрібен, оскільки я працюю в каталозі BEETROOT_128, а файл mymod міститься в підкаталозі Home_work_9, і його не знаходить. Рішення є два, або прописувати шлях до файлу, або скопіювати до каталогу з якого я працюю. я не став заморачуватися з import os і скопіював файл.
+#first_channel() - turns on the first channel from the list.
+#last_channel() - turns on the last channel from the list.
+#turn_channel(N) - turns on the N channel. Pay attention that the channel numbers start from 1, not from 0.
+#next_channel() - turns on the next channel. If the current channel is the last one, turns on the first channel.
+#previous_channel() - turns on the previous channel. If the current channel is the first one, turns on the last channel.
+#current_channel() - returns the name of the current channel.
+#exists(N/'name') - gets 1 argument - the number N or the string 'name' and returns "Yes", if the channel N or 'name' exists in the list, or "No" - in the other case.
+ 
 
-#Try running your module on itself: e.g., test("mymod.py"). Note that the test opens the file twice; if you’re feeling ambitious, you may be able to improve this by passing an open file object into the two count functions (hint: file.seek(0) is a file rewind).
-# для цього доведеться 2 функції об'єднати в одну, і використати file.seek(0) для повернення на нульовий індекс 
+#The default channel turned on before all commands is №1.
+
+#Your task is to create the TVController class and methods described above.
+
+CHANNELS = ["BBC", "Discovery", "TV1000"]
+
+class TVController():
+    def __init__(self, channel):
+        self.channel = channel
+        self.channel_now = list()
+         
+    def first_channel(self):
+        self.channel_now.append(self.channel[0])
+        return self.channel[0]
+
+    def last_channel(self):
+        self.channel_now = self.channel[-1]
+        return self.channel[-1]
+
+    def turn_channel(self,n):
+        if n == 1:
+            self.channel_now = self.channel[0]
+            return self.channel[0]
+        if n == 2:
+            self.channel_now = self.channel[1]
+            return self.channel[1]
+        if n == 3:
+            self.channel_now = self.channel[2]
+            return self.channel[2]
+        else:
+               print("Channels not exist")
+
+    def next_channel(self):
+        if self.channel_now == self.channel[0]:
+            self.channel_now = self.channel[1]
+            return self.channel_now
+        if self.channel_now == self.channel[1]:
+            self.channel_now = self.channel[2]
+            return self.channel_now
+        if self.channel_now == self.channel[2]:
+            self.channel_now = self.channel[0]
+            return self.channel_now
+
+
+    def previous_channel(self):
+        if self.channel_now == self.channel[2]:
+            self.channel_now = self.channel[1]
+            return self.channel_now
+        if self.channel_now == self.channel[1]:
+            self.channel_now = self.channel[0]
+            return self.channel_now
+        if self.channel_now == self.channel[0]:
+            self.channel_now = self.channel[2]
+            return self.channel_now
+        
+    def current_channel(self):
+          return self.channel_now
+    
+    def exists(self,arg):
+        if isinstance(arg, str):
+            if arg in CHANNELS:
+                return "Yes"
+            else:
+                return "No"
+        if isinstance(arg, int):
+            if arg <= 3 and arg >= 1:
+                return "Yes"
+            else:
+                return "No"
+        else: print("Not found")
+
+controller = TVController(CHANNELS)
+controller.first_channel() == "BBC"
+controller.last_channel() == "TV1000"
+controller.turn_channel(1) == "BBC"
+controller.next_channel() == "Discovery"
+controller.previous_channel() == "BBC"
+controller.current_channel() == "BBC"
+controller.exists(4) == "No"
+controller.exists("BBC") == "Yes"
